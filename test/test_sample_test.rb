6b62a4048274101f7f1fcde4b927c0e4dd11a59e
Koichi Sasada <ko1@atdot.net>
2026-02-08 18:37:30 +0900

    demo

--- test/test_sample_test.rb
+++ test/test_sample_test.rb
@@ -16,6 +16,10 @@ class SampleProjectTest < Minitest::Test
     assert_equal 420, (30 + 3 * 4) * 10
   end
 
+  def test_hoge
+    assert_equal 4200, (30 + 3 * 4) * 100
+  end
+
   def test_baz
     assert_equal 43, 30 + 3 * 4 + 1
   end
